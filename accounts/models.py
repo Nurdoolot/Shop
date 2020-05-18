2320dcbc9e38bab64aa8780b0bac0b3f7bce964c
Nurdoolot Sonunbekov <rayfi.vv@gmail.com>
2020-05-18 22:12:58 +0600

    Model for Profile

---
+++ accounts/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, blank=True, null=True)
+    first_name = models.CharField(max_length=100, null=True, blank=True)
+    last_name = models.CharField(max_length=100, null=True, blank=True)
+    email = models.EmailField(null=True, blank=True)
+    photo = models.ImageField(upload_to='users/', default='images.png')
+    def __str__(self):
+        return str(self.user)
