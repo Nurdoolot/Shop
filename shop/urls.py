71d2f4c3f82c2894125b2d7f3b5b5acb02ef8227
Nurdoolot Sonunbekov <rayfi.vv@gmail.com>
2020-05-19 02:05:06 +0600

    .

--- shop/urls.py
+++ shop/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import product, detail, product_create, product_delete, product_update
+from .views import product, detail, product_create, product_delete, product_update, like_post
 
 
 urlpatterns = [
     path('', product, name='product'),
+    path('like/', like_post, name='like'),
     path('create/', product_create, name='create'),
     path('update/<int:pk>/', product_update, name='update'),
     path('delete/<int:pk>/', product_delete, name='delete'),
