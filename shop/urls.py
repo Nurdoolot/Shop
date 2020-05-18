868bab3ccf328a0c20d75035790ee70eadc38f58
Nurdoolot Sonunbekov <rayfi.vv@gmail.com>
2020-05-18 22:11:21 +0600

    Path for category and product

---
+++ shop/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import product, detail, product_create, product_delete, product_update
+
+
+urlpatterns = [
+    path('', product, name='product'),
+    path('create/', product_create, name='create'),
+    path('update/<int:pk>/', product_update, name='update'),
+    path('delete/<int:pk>/', product_delete, name='delete'),
+    path('<slug:slug>/', product, name='product_by_category'),
+    path('<int:id>/<slug:slug>/', detail, name='detail'),
+]
